ede00c84f5de461a76f3e10c730f189f02fbad89
Howard Kennedy <howardkennedy@howards-mbp-2.lan>
2022-02-09 11:22:21 -0600

    nav and revisions

--- script.js
+++ script.js
@@ -1,40 +1,60 @@
-// $(document).ready(() => {
-    const intersectionSections = Array.from(document.getElementsByClassName('intersection-section'));
-    const options = {
-        root: null,
-        threshold: 0.5,
-        // rootMargin: "-150px"
-    };
+const intersectionSections = Array.from(document.getElementsByClassName('intersection-section'));
+const options = {
+    root: null,
+    threshold: 0.5,
+    // rootMargin: "-25px"
+};
 
-    console.log(intersectionSections)
+const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            const animateEls = $(entry.target).find('.animate');
 
-    const observer = new IntersectionObserver(function (entries, observer) {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            } else {
-                console.log("I'm here");
-                const animateEls = $(entry.target).find('.animate');
+            animateEls.each((i, el) => {
+                if ($(el).attr('data-delay')) {
+                    $(el).css('transition-delay', `${$(el).attr('data-delay')}`);
+                }
 
-                animateEls.each((i, el) => {
-                    if($(el).attr('data-delay')) {
-                        $(el).css('transition-delay', `${$(el).attr('data-delay')}`);
-                    } 
+                if ($(el).attr('data-width')) {
+                    $(el).css('width', `${$(el).attr('data-width')}`);
+                }
 
-                    if($(el).attr('data-width')) {
-                        $(el).css('width', `${$(el).attr('data-width')}`);
-                    }
-                    
-                    $(el).addClass('activate');
-                })
+                $(el).addClass('activate');
+                observer.unobserve(entry.target);
+            })
 
-                // animateEls.addClass('activate');
-            }
+           
+            // animateEls.addClass('activate');
+        }
 
-        })
-    }, options);
+    })
+}, options);
 
-    intersectionSections.forEach(section => {
-        observer.observe(section);
-    });
-// })
+intersectionSections.forEach(section => {
+    observer.observe(section);
+});
+
+$('input, textarea').focus((e) => {
+    console.log($(e.target).siblings())
+    $('#contact-form').find('.underline').removeClass('focused');
+    $(e.target).siblings().addClass('focused')
+
+});
+
+// $('.navbar-nav-link').click((e) => {
+//     const sectionId = $(e.target).attr('data-navigation');
+//     const padOffset = '10rem'
+
+//     e.preventDefault();
+
+//     $('html, body').animate({
+//         scrollTop: ($(`#${sectionId}-section`).offset().top + padOffset)
+//       }, 500);
+
+// });
+
+$('.project').mouseleave(() => {
+    console.log('now')
+})
